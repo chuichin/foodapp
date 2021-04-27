6486b1f4d7a9cf113952dad01741ccbbe67f6982
chuichin <chuichin.code@gmail.com>
2021-04-27 16:36:02 +0800

    fix secret key

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or os.urandom(32)
+        'SECRET_KEY')
     S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
     S3_KEY = os.environ.get("S3_ACCESS_KEY")
     S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
