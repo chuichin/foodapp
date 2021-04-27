727163ecc53d6dff7ce732e39b018d875697fcfe
chuichin <chuichin.code@gmail.com>
2021-04-27 17:56:41 +0800

    .

--- foodapp_api/blueprints/menu_images/views.py
+++ foodapp_api/blueprints/menu_images/views.py
@@ -3,10 +3,33 @@ from app import app, s3
 import boto3, botocore
 from models.menu_image import MenuImage
 from models.chef_menu import ChefMenu
+from models.chef import Chef
 
 from flask import Flask, request, jsonify, Blueprint
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt_header
+
 
 menu_images_api_blueprint = Blueprint('menu_images_api', __name__)
+
+
+
+# POST /menu_images/new
+@menu_images_api_blueprint.route('/new/<chef_menu_id>', methods=["POST"])
+@jwt_required()
+def new_menu_image(chef_menu_id): 
+    existing_chef = Chef.get_or_none(Chef.email == get_jwt_identity())
+    if existing_chef:
+        image_path = request.json.get("image_url", None)
+        # breakpoint()
+        new_menu_image = MenuImage(chef=existing_chef.id, chef_menu=chef_menu_id, image_path=image_path)
+        if new_menu_image.save():
+            return jsonify({
+                "message": "Successfully posted this menu's image",
+                "chef_id": new_menu_image.chef_id,
+                "menu_id": new_menu_image.chef_menu_id,
+                "image_path": new_menu_image.image_path,
+            }), 200
+
 
 # GET /menu_images - Return list of menu images
 @menu_images_api_blueprint.route('/<chef_menu_id>', methods=["GET"])
@@ -17,40 +40,43 @@ def index(chef_menu_id):
             "menu_images_id" : each.id,
             "chef" : each.chef_id, 
             "menu_id": each.chef_menu_id,
-            "image_url": each.image_url
+            "image_path": each.image_path
         } for each in menu_images]
         return jsonify(menu_images), 200
     else:
         return jsonify([]), 200
 
-# POST /menu_images/new
-@menu_images_api_blueprint.route('/new/<chef_menu_id>', methods=["POST"])
-def new_menu_image(chef_menu_id): 
-    if request.content_length == 0:
+# UPDATE /menu_images
+@menu_images_api_blueprint.route("/update/<chef_menu_image_id>", methods=["PUT"])
+@jwt_required()
+def update_image(chef_menu_image_id):
+    if get_jwt_header()['type'] == "Chef":
+        chef = Chef.get_or_none(Chef.email == get_jwt_identity())
+        if request.content_length == 0:
             return jsonify(message="No images passed", status="failed"), 400
-    elif request.files['menu_image']:
-        file = request.files.get('menu_image')
-        s3.upload_fileobj(
-            file,
-            "foodapp-new",
-            f"menus/{chef_menu_id}/{file.filename}",
-            ExtraArgs={
-                "ACL": "public-read",
-                "ContentType": file.content_type
-            }
-        )
-        existing_chef = ChefMenu.get_or_none(ChefMenu.id == chef_menu_id)
-        new_menu_image = MenuImage(chef=existing_chef.chef_id, chef_menu=chef_menu_id, image_path=f"https://foodapp-new.s3-ap-southeast-1.amazonaws.com/menus/{chef_menu_id}/{file.filename}")
-        if new_menu_image.save(): 
-            new_menu_image = MenuImage.get_or_none(MenuImage.chef_menu == chef_menu_id)
+        elif request.files['menu_image']:
+            file = request.files.get('menu_image')
+            menu_image = MenuImage.get(MenuImage.chef == chef.id)
+            s3.upload_fileobj(
+                file,
+                "foodapp-new",
+                f"menus/{menu_image.chef_id}/{file.filename}",
+                ExtraArgs={
+                    "ACL": "public-read",
+                    "ContentType": file.content_type
+                }
+            )
+            update = MenuImage.update({MenuImage.image_path:f"https://foodapp-new.s3-ap-southeast-1.amazonaws.com/menus/{menu_image.chef_id}/{file.filename}"}).where(MenuImage.id == chef_menu_image_id).execute()
+            updated_image = MenuImage.get(MenuImage.id == chef_menu_image_id)
             return jsonify({
-                "message": "Successfully posted this menu's image",
-                "chef_id": new_menu_image.chef_id,
-                "menu_id": new_menu_image.chef_menu_id,
-                "image": new_menu_image.image_path,
-            }), 200 
-    
-    
+                "message": "Successfully updated menu image",
+                "menu_image_id": updated_image.id,
+                "image": updated_image.image_path,
+                "chef_id": updated_image.chef_id
+            }), 200
+    else:
+        return jsonify(message="You are not logged in as Chef"), 400
+   
     
 # DELETE /menu_images/delete/<menu_images_id>
 @menu_images_api_blueprint.route('/delete/<menu_image_id>', methods=["DELETE"])
